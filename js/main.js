db1a219e4dbe4de55ed207ac46715b9dfbb3f6bc
Dago R <dagoramirezserna@gmail.com>
2020-07-27 10:34:57 -0500

    Se crea el archivo js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
-function multiplicar(){
+// document.getElementById("enviar").addEventListener("click", multiplicar);
+document.getElementById("cantidad").addEventListener("change", multiplicar);
+document.getElementById("valor-unitario").addEventListener("keyup", multiplicar);
+function multiplicar(evento) {
     const cantidad = document.getElementById("cantidad").value;
     const vrUnitario = document.getElementById("valor-unitario").value;
-    const resultado = cantidad*vrUnitario;
+    const resultado = cantidad*vrUnitario;    
     document.getElementById("valor-total").value = resultado;
+    //Condicional para hacer que la página se recargue sólo cuando se da click
+    // if(evento.type == "click") {
+    //     evento.preventDefault();
+    // }
 }
