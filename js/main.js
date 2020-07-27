4410882f85a5187383087ba14c078e9296566252
Dago R <dagoramirezserna@gmail.com>
2020-07-27 02:29:07 -0500

    Se crea el archivo cotizador html

---
+++ js/main.js
@@ -0,0 +1,4 @@
+const cantidad = document.getElementById("cantidad").value;
+const vrUnitario = document.getElementById("valor-unitario").value;
+const resultado = cantidad*vrUnitario;
+document.getElementById("valor-total").value = resultado;
